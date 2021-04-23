01cde86bf82553ad876846cb65474b737754fbfc
batuhankrbb <batuhankarababa@gmail.com>
2021-04-23 12:24:08 +0300

    add: Activity Entity | update: migrate to null safety

---
+++ lib/features/activities/domain/entities/acticity_entity.dart
@@ -0,0 +1,8 @@
+class ActivityEntity {
+  final String activity;
+  final String type;
+  final String price;
+  final double accessibility;
+
+  ActivityEntity({required this.activity, required this.type, required this.price, required this.accessibility});
+}

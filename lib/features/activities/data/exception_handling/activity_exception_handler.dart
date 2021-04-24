a4c793e7ef6be0c955940bc1c5fa53b27f1a4dff
batuhankrbb <batuhankarababa@gmail.com>
2021-04-24 15:57:50 +0300

    add: ActivityExceptions, ActivityExceptionHandler, error handling to ActivityRepositoryImplementation

---
+++ lib/features/activities/data/exception_handling/activity_exception_handler.dart
@@ -0,0 +1,20 @@
+import 'package:im_bored_app/core/error_handling/custom_failure.dart';
+import 'package:im_bored_app/features/activities/data/data_sources/exceptions/activity_exceptions.dart';
+
+class ActivityExceptionHandler {
+  CustomFailure handleException(ActivityException exception) {
+    if (exception is UnknownLocalException) {
+      return CustomFailure(message: "Oh! Something went wrong in Local :(");
+    } else if (exception is UnknownRemoteException) {
+      return CustomFailure(message: "Oh! Something went wrong in Remote :(");
+    } else if (exception is ActivityZeroFavoriteException) {
+      return CustomFailure(
+          message:
+              "Oh! You have no internet connection and It's your first time.");
+    } else if (exception is NoInternetException) {
+      return CustomFailure(message: "It seems like you have some connection problems.");
+    } else {
+      throw Exception("App Crashed in ActivityExceptionHandler");
+    }
+  }
+}
--- lib/features/activities/data/repositories/activity_repository_implementation.dart
+++ lib/features/activities/data/repositories/activity_repository_implementation.dart
@@ -1,7 +1,10 @@
+import 'package:im_bored_app/core/error_handling/custom_failure.dart';
 import 'package:im_bored_app/core/network_checking/network_checker_contract.dart';
 import 'package:im_bored_app/core/result_types/result.dart';
 import 'package:im_bored_app/features/activities/data/data_sources/contracts/local_data_source_contract.dart';
 import 'package:im_bored_app/features/activities/data/data_sources/contracts/remote_data_source_contract.dart';
+import 'package:im_bored_app/features/activities/data/data_sources/exceptions/activity_exceptions.dart';
+import 'package:im_bored_app/features/activities/data/exception_handling/activity_exception_handler.dart';
 import 'package:im_bored_app/features/activities/data/models/activity_model.dart';
 import 'package:im_bored_app/features/activities/domain/entities/activity_entity.dart';
 import 'package:im_bored_app/features/activities/domain/repositories/activity_repository.dart';
@@ -10,37 +13,56 @@ class ActivityRepositoryImplementation implements ActivityRepositoryContract {
   final ActivityLocalDataSourceContract localDataSourceContract;
   final ActivityRemoteDataSourceContract remoteDataSourceContract;
   final NetworkCheckerContract networkCheckerContract;
+  final ActivityExceptionHandler exceptionHandler;
 
   ActivityRepositoryImplementation(
       {required this.localDataSourceContract,
       required this.remoteDataSourceContract,
-      required this.networkCheckerContract});
+      required this.networkCheckerContract,
+      required this.exceptionHandler});
 
   @override
   Future<Result<ActivityEntity>> getActivityByType(String type) async {
-    return fetchFromNetworkWithNetworkChecking(() {
-      return remoteDataSourceContract.getActivityByType(type);
-    });
+    try {
+      return _fetchFromNetworkWithNetworkChecking(() {
+        return remoteDataSourceContract.getActivityByType(type);
+      });
+    } on ActivityException catch (e) {
+      return Result.failure(exceptionHandler.handleException(e));
+    }
   }
 
   @override
   Future<Result<ActivityEntity>> getRandomActivity() async {
-    return fetchFromNetworkWithNetworkChecking(() {
-      return remoteDataSourceContract.getRandomActivity();
-    });
+    try {
+      return _fetchFromNetworkWithNetworkChecking(() {
+        return remoteDataSourceContract.getRandomActivity();
+      });
+    } on ActivityException catch (e) {
+      return Result.failure(exceptionHandler.handleException(e));
+    }
   }
 
   @override
   Future<Result<List<ActivityEntity>>> getFavoriteActivities() async {
-    var activities = await localDataSourceContract.getFavoriteActivities();
-    return Success(activities.map((e) => e.toEntity()).toList());
+    try {
+      var activities = await localDataSourceContract
+          .getFavoriteActivities(); //check error handling -type 1
+      return Success(activities.map((e) => e.toEntity()).toList());
+    } on ActivityException catch (e) {
+      return Result.failure(exceptionHandler.handleException(e));
+    }
   }
 
   @override
-  Future<bool> saveActivityAsFavorite(ActivityEntity activity) async {
-    var result =
-        await localDataSourceContract.saveActivityAsFavorite(activity.model);
-    return result;
+  Future<Result<bool>> saveActivityAsFavorite(ActivityEntity activity) async {
+    try {
+      var result =
+          await localDataSourceContract.saveActivityAsFavorite(activity.model);
+      return Result.success(result);
+    } on ActivityException catch (e) {
+      return Result.failure(exceptionHandler.handleException(e));
+    }
   }
 
   Future<ActivityModel> _getLastFavorite() async {
@@ -48,13 +70,14 @@ class ActivityRepositoryImplementation implements ActivityRepositoryContract {
     return lastFavorite;
   }
 
-  Future<Result<ActivityEntity>> fetchFromNetworkWithNetworkChecking(
+  Future<Result<ActivityEntity>> _fetchFromNetworkWithNetworkChecking(
       Future<ActivityModel> Function() getActivity) async {
     if (await networkCheckerContract.isConnected()) {
-      var activity = await getActivity();
+      var activity = await getActivity(); //check error handling -type 2
       return Result<ActivityEntity>.success(activity.toEntity());
     } else {
-      var activity = await _getLastFavorite();
+      var activity =
+          await _getLastFavorite(); //it's already checked in _getLastFavorite
       return Result<ActivityEntity>.success(activity.toEntity());
     }
   }

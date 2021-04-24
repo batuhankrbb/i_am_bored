662459e4913899ba01d5fa8835a46b1ecb3940a8
batuhankrbb <batuhankarababa@gmail.com>
2021-04-24 07:55:34 +0300

    add: getLastFavoriteActivity() to ActivityLocalDataSource, network checking to ActivityRepositoryImplementation

--- lib/features/activities/data/data_sources/contracts/local_data_source_contract.dart
+++ lib/features/activities/data/data_sources/contracts/local_data_source_contract.dart
@@ -3,4 +3,5 @@ import 'package:im_bored_app/features/activities/data/models/activity_model.dart
 abstract class ActivityLocalDataSourceContract {
   Future<List<ActivityModel>> getFavoriteActivities();
   Future<bool> saveActivityAsFavorite(ActivityModel model);
+  Future<ActivityModel> getLastFavoriteActivity();
 }
--- lib/features/activities/data/repositories/activity_repository_implementation.dart
+++ lib/features/activities/data/repositories/activity_repository_implementation.dart
@@ -1,23 +1,33 @@
+import 'package:im_bored_app/core/network_checking/network_checker_contract.dart';
 import 'package:im_bored_app/core/result_types/result.dart';
 import 'package:im_bored_app/features/activities/data/data_sources/contracts/local_data_source_contract.dart';
 import 'package:im_bored_app/features/activities/data/data_sources/contracts/remote_data_source_contract.dart';
+import 'package:im_bored_app/features/activities/data/models/activity_model.dart';
 import 'package:im_bored_app/features/activities/domain/entities/activity_entity.dart';
 import 'package:im_bored_app/features/activities/domain/repositories/activity_repository.dart';
 
 class ActivityRepositoryImplementation implements ActivityRepositoryContract {
   final ActivityLocalDataSourceContract localDataSourceContract;
   final ActivityRemoteDataSourceContract remoteDataSourceContract;
+  final NetworkCheckerContract networkCheckerContract;
 
-  ActivityRepositoryImplementation({
-    required this.localDataSourceContract,
-    required this.remoteDataSourceContract,
-  });
+  ActivityRepositoryImplementation(
+      {required this.localDataSourceContract,
+      required this.remoteDataSourceContract,
+      required this.networkCheckerContract});
 
   @override
   Future<Result<ActivityEntity>> getActivityByType(String type) async {
-    var activity = await remoteDataSourceContract
-        .getActivityByType(type); //do error handling
-    return Result<ActivityEntity>.success(activity.toEntity());
+    return fetchFromNetworkWithNetworkChecking(() {
+      return remoteDataSourceContract.getActivityByType(type);
+    });
+  }
+
+  @override
+  Future<Result<ActivityEntity>> getRandomActivity() async {
+    return fetchFromNetworkWithNetworkChecking(() {
+      return remoteDataSourceContract.getRandomActivity();
+    });
   }
 
   @override
@@ -27,15 +37,25 @@ class ActivityRepositoryImplementation implements ActivityRepositoryContract {
   }
 
   @override
-  Future<Result<ActivityEntity>> getRandomActivity() async {
-    var activity = await remoteDataSourceContract.getRandomActivity();
-    return Success(activity.toEntity());
-  }
-
-  @override
   Future<bool> saveActivityAsFavorite(ActivityEntity activity) async {
     var result =
         await localDataSourceContract.saveActivityAsFavorite(activity.model);
     return result;
+  }
+
+  Future<ActivityModel> _getLastFavorite() async {
+    var lastFavorite = await localDataSourceContract.getLastFavoriteActivity();
+    return lastFavorite;
+  }
+
+  Future<Result<ActivityEntity>> fetchFromNetworkWithNetworkChecking(
+      Future<ActivityModel> Function() getActivity) async {
+    if (await networkCheckerContract.isConnected()) {
+      var activity = await getActivity();
+      return Result<ActivityEntity>.success(activity.toEntity());
+    } else {
+      var activity = await _getLastFavorite();
+      return Result<ActivityEntity>.success(activity.toEntity());
+    }
   }
 }

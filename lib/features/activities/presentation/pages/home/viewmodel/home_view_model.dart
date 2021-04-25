16cadb69d7904bcd9f97aad5569eed910f85c7e5
batuhankrbb <batuhankarababa@gmail.com>
2021-04-25 10:00:21 +0300

    add: StateResult abstract class by @freezed, HomeViewModel

---
+++ lib/features/activities/presentation/pages/home/viewmodel/home_view_model.dart
@@ -0,0 +1,51 @@
+import 'package:im_bored_app/core/result_types/state_result.dart';
+import 'package:im_bored_app/features/activities/domain/entities/activity_entity.dart';
+import 'package:im_bored_app/features/activities/domain/usecases/activities_usecases.dart';
+import 'package:mobx/mobx.dart';
+part 'home_view_model.g.dart';
+
+class HomeViewModel = _HomeViewModel with _$HomeViewModel;
+
+abstract class _HomeViewModel with Store {
+  final GetRandomActivity getRandomActivityUseCase;
+  final GetActivityByType getActivityByTypeUseCase;
+  final SaveActivityAsFavorite saveActivityAsFavoriteUseCase;
+  final GetFavoriteActivities getFavoriteActivitiesUseCase;
+
+  _HomeViewModel(
+      {required this.getRandomActivityUseCase,
+      required this.getActivityByTypeUseCase,
+      required this.saveActivityAsFavoriteUseCase,
+      required this.getFavoriteActivitiesUseCase});
+
+  @action
+  Future<void> getActivityByType(String type) async {
+    activityStateResult = StateResult.loading();
+    var result = await getActivityByTypeUseCase
+        .execute(type); //bu şekilde diğer metodu da doldur
+    result.when(success: (data) {
+      activityStateResult = StateResult.completed(data);
+    }, failure: (failure) {
+      activityStateResult = StateResult.failed(failure);
+    });
+  }
+
+  @action
+  Future<void> getRandomActivity() async {
+    activityStateResult = StateResult.loading();
+    var result = await getRandomActivityUseCase.execute();
+    result.when(success: (data) {
+      activityStateResult = StateResult.completed(data);
+    }, failure: (failure) {
+      activityStateResult = StateResult.failed(failure);
+    });
+  }
+
+  @observable
+  StateResult<ActivityEntity> activityStateResult = StateResult.initial();
+}
+
+//getactivitybytype
+//getrandomactivity
+////activityresultstate
+///types will be enum. I will change all types and make it string in repository layer
---
+++ lib/features/activities/presentation/pages/home/viewmodel/home_view_model.g.dart
@@ -0,0 +1,51 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'home_view_model.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$HomeViewModel on _HomeViewModel, Store {
+  final _$activityStateResultAtom =
+      Atom(name: '_HomeViewModel.activityStateResult');
+
+  @override
+  StateResult<ActivityEntity> get activityStateResult {
+    _$activityStateResultAtom.reportRead();
+    return super.activityStateResult;
+  }
+
+  @override
+  set activityStateResult(StateResult<ActivityEntity> value) {
+    _$activityStateResultAtom.reportWrite(value, super.activityStateResult, () {
+      super.activityStateResult = value;
+    });
+  }
+
+  final _$getActivityByTypeAsyncAction =
+      AsyncAction('_HomeViewModel.getActivityByType');
+
+  @override
+  Future<void> getActivityByType(String type) {
+    return _$getActivityByTypeAsyncAction
+        .run(() => super.getActivityByType(type));
+  }
+
+  final _$getRandomActivityAsyncAction =
+      AsyncAction('_HomeViewModel.getRandomActivity');
+
+  @override
+  Future<void> getRandomActivity() {
+    return _$getRandomActivityAsyncAction.run(() => super.getRandomActivity());
+  }
+
+  @override
+  String toString() {
+    return '''
+activityStateResult: ${activityStateResult}
+    ''';
+  }
+}

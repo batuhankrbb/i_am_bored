441f9eb6f36ab810e64928706582e136609885d9
batuhankrbb <batuhankarababa@gmail.com>
2021-04-29 06:54:59 +0300

    add: InjectionContainer for GetIt, setUpHive for Hive

---
+++ lib/starting_files/get_it_injection_container.dart
@@ -0,0 +1,79 @@
+import 'package:connectivity/connectivity.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:im_bored_app/core/network/network_checking/network_checker_contract.dart';
+import 'package:im_bored_app/core/network/network_checking/network_checker_implementation.dart';
+import 'package:im_bored_app/features/activities/data/data_sources/contracts/local_data_source_contract.dart';
+import 'package:im_bored_app/features/activities/data/data_sources/contracts/remote_data_source_contract.dart';
+import 'package:im_bored_app/features/activities/data/data_sources/implementations/local_data_source_implementation.dart';
+import 'package:im_bored_app/features/activities/data/data_sources/implementations/remote_data_source_implementation.dart';
+import 'package:im_bored_app/features/activities/data/exception_handling/handler/activity_exception_handler.dart';
+import 'package:im_bored_app/features/activities/data/repositories/activity_repository_implementation.dart';
+import 'package:im_bored_app/features/activities/domain/repositories/activity_repository.dart';
+import 'package:im_bored_app/features/activities/domain/usecases/activities_usecases.dart';
+import 'package:im_bored_app/features/activities/presentation/screens/favorites/viewmodel/favorites_viewmodel.dart';
+import 'package:im_bored_app/features/activities/presentation/screens/home/viewmodel/home_view_model.dart';
+
+GetIt getit = GetIt.instance;
+
+void setUpGetIt() {
+  _setUpViewModels();
+  _setUpUseCases();
+  _setUpRepositories();
+  _setUpDataSources();
+  _setUpExternalSources();
+}
+
+void _setUpViewModels() {
+  getit.registerSingleton(({required BuildContext context}) => HomeViewModel(
+      getActivityByTypeUseCase: getit(),
+      getRandomActivityUseCase: getit(),
+      saveActivityAsFavoriteUseCase: getit(),
+      deleteFavoriteActivityUseCase: getit(),
+      context: context));
+  getit.registerSingleton(({required BuildContext context}) =>
+      FavoritesViewModel(
+          getFavoriteActivitiesUseCase: getit(),
+          deleteFavoriteActivityUseCase: getit(),
+          context: context));
+}
+
+void _setUpUseCases() {
+  getit.registerLazySingleton(
+      () => GetRandomActivity(repositoryContract: getit()));
+
+  getit.registerLazySingleton(
+      () => GetActivityByType(repositoryContract: getit()));
+  getit.registerLazySingleton(
+      () => GetFavoriteActivities(repositoryContract: getit()));
+  getit.registerLazySingleton(
+      () => SaveActivityAsFavorite(repositoryContract: getit()));
+  getit.registerLazySingleton(
+      () => DeleteFavoriteActivity(repositoryContract: getit()));
+}
+
+void _setUpRepositories() {
+  getit.registerLazySingleton<ActivityRepositoryContract>(() =>
+      ActivityRepositoryImplementation(
+          localDataSourceContract: getit(),
+          remoteDataSourceContract: getit(),
+          networkCheckerContract: getit(),
+          exceptionHandler: getit()));
+}
+
+void _setUpDataSources() {
+  getit.registerLazySingleton<ActivityLocalDataSourceContract>(
+      () => ActivityLocalDataSourceImplementation());
+
+  getit.registerLazySingleton<ActivityRemoteDataSourceContract>(
+      () => ActivityRemoteDataSourceImplementation());
+
+  getit.registerLazySingleton<NetworkCheckerContract>(
+      () => NetworkCheckerImplementation(connectivity: getit()));
+
+  getit.registerLazySingleton(() => ActivityExceptionHandler());
+}
+
+void _setUpExternalSources() {
+  getit.registerLazySingleton(() => Connectivity());
+}
